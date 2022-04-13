ac80bc351739c5e315b264ac4b8e458451f9ae4e
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-04-13 20:15:57 +0300

    feat(mcatal): Added alerts

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from 'react';
 import './EditMovie.css';
 import Input from './form-components/Input';
 import TextArea from './form-components/TextArea';
-import Select from './form-components/Select'
+import Select from './form-components/Select';
+import Alert from './ui-components/Alert'
 
 export default class EditMovie extends Component {
 	constructor(props) {
@@ -27,6 +28,10 @@ export default class EditMovie extends Component {
 			isLoaded: false,
 			error: null,
 			errors: [],
+			alert: {
+				type: "d-none",
+				message: "",
+			}
 		}
 
 		this.handleChange = this.handleChange.bind(this);
@@ -72,7 +77,15 @@ export default class EditMovie extends Component {
 		fetch('http://localhost:8080/v1/admin/editmovie', requestOptions)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
+				if(data.error) {
+					this.setState({
+						alert: {type: "alert-danger", message: data.error.message},
+					});
+				} else {
+					this.setState({
+						alert: {type: "alert-success", message: "Changes saved!"},
+					});
+				}
 			})
 	};
 
@@ -143,6 +156,7 @@ export default class EditMovie extends Component {
 			return (
 				<Fragment>
 					<h2>Add/Edit Movie</h2>
+					<Alert alertType={this.state.alert.type} alertMessage={this.state.alert.message} />
 					<hr/>
 					<form onSubmit={this.handleSubmit}>
 						<input type="hidden" name="id" id="id" value={movie.id} onChange={this.handleChange}/>
@@ -188,10 +202,6 @@ export default class EditMovie extends Component {
 
 						<button className="btn btn-primary">Save</button>
 					</form>
-
-					<div className="mt-3">
-						<pre>{JSON.stringify(this.state, null, 3)}</pre>
-					</div>
 				</Fragment>
 			)
 		}
