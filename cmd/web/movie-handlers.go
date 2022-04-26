aaff601d535b7d65a399357c065fc50ed1a36a96
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-04-26 19:39:03 +0300

    feat(editMovie): Now edit movie can edit movies xd

--- cmd/web/movie-handlers.go
+++ cmd/web/movie-handlers.go
@@ -110,6 +110,23 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	var movie models.Movie
 
+	if payload.ID != "0" {
+		id, err := strconv.Atoi(payload.ID)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		m, err := app.models.DB.Get(id)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
 	movie.ID, err = strconv.Atoi(payload.ID)
 	if err != nil {
 		app.errorJSON(w, err)
@@ -140,6 +157,12 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	if movie.ID == 0 {
 		err = app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
 		if err != nil {
 			app.errorJSON(w, err)
 			return
